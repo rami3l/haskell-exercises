df2bff0a0cff05d497216c3483c48690a147173a
rami3l <rami3l@outlook.com>
2022-05-27 03:05:16 +0200

    feat: (wip) solve `06-TypeFamilies`, pt. 2

--- 05-RankNTypes/src/Exercises.hs
+++ 05-RankNTypes/src/Exercises.hs
@@ -21,8 +21,9 @@ unpackExlistential :: Exlistential -> (forall a. a -> r) -> [r]
 unpackExlistential Nil _ = []
 unpackExlistential (Cons a as) f = f a : unpackExlistential as f
 
--- | b. Regardless of which type @r@ actually is, what can we say about the
--- values in the resulting list?
+{- | b. Regardless of which type @r@ actually is, what can we say about the
+ values in the resulting list?
+-}
 
 -- The signature of the function @f@ is too general. Only a few functions apply,
 -- eg. 'const someR'
@@ -33,8 +34,9 @@ unpackExlistential (Cons a as) f = f a : unpackExlistential as f
 
 {- TWO -}
 
--- | Consider the following GADT that existentializes a 'Foldable' structure
--- (but, crucially, not the type inside).
+{- | Consider the following GADT that existentializes a 'Foldable' structure
+ (but, crucially, not the type inside).
+-}
 data CanFold a where
   CanFold :: Foldable f => f a -> CanFold a
 
@@ -42,8 +44,9 @@ data CanFold a where
 unpackCanFold :: (forall f. Foldable f => f a -> r) -> CanFold a -> r
 unpackCanFold f (CanFold x) = f x
 
--- | b. Can we use 'unpackCanFold' to figure out if a 'CanFold' is "empty"?
--- Could we write @length :: CanFold a -> Int@? If so, write it!
+{- | b. Can we use 'unpackCanFold' to figure out if a 'CanFold' is "empty"?
+ Could we write @length :: CanFold a -> Int@? If so, write it!
+-}
 flength :: CanFold a -> Int
 flength = unpackCanFold length
 
@@ -56,50 +59,57 @@ instance Foldable CanFold where
 -- | Recall our existential 'EqPair' GADT:
 data EqPair where EqPair :: Eq a => a -> a -> EqPair
 
--- | a. Write a function that "unpacks" an 'EqPair' by applying a user-supplied
--- function to its pair of values in the existential type.
+{- | a. Write a function that "unpacks" an 'EqPair' by applying a user-supplied
+ function to its pair of values in the existential type.
+-}
 unpackEqPair :: (forall a. Eq a => a -> a -> r) -> EqPair -> r
 unpackEqPair faar (EqPair a a') = faar a a'
 
--- | b. Write a function that takes a list of 'EqPair's and filters it
--- according to some predicate on the unpacked values.
+{- | b. Write a function that takes a list of 'EqPair's and filters it
+ according to some predicate on the unpacked values.
+-}
 epfilter :: (forall a. Eq a => a -> a -> Bool) -> [EqPair] -> [EqPair]
 epfilter f = filter (unpackEqPair f)
 
--- | c. Write a function that unpacks /two/ 'EqPair's. Now that both our
--- variables are in rank-2 position, can we compare values from different
--- pairs?
+{- | c. Write a function that unpacks /two/ 'EqPair's. Now that both our
+ variables are in rank-2 position, can we compare values from different
+ pairs?
+-}
 unpackEqPairs :: (forall a. Eq a => a -> a -> r) -> EqPair -> EqPair -> (r, r)
 unpackEqPairs f xs ys = (unpackEqPair f xs, unpackEqPair f ys)
 
 {- FOUR -}
 
--- | When I was building @purescript-panda@, I came across a neat use case for
--- rank-2 types. Consider the following sketch of a type:
+{- | When I was building @purescript-panda@, I came across a neat use case for
+ rank-2 types. Consider the following sketch of a type:
+-}
 data Component input output
 
 -- = Some sort of component stuff.
 
--- | Now, let's imagine we want to add a constructor to "nest" a component
--- inside another component type. We need a way of transforming between our
--- "parent" I/O and "child" I/O, so we write this type:
+{- | Now, let's imagine we want to add a constructor to "nest" a component
+ inside another component type. We need a way of transforming between our
+ "parent" I/O and "child" I/O, so we write this type:
+-}
 data Nested input output subinput suboutput = Nested
-  { inner :: Component subinput suboutput,
-    input :: input -> subinput,
-    output :: suboutput -> output
+  { inner :: Component subinput suboutput
+  , input :: input -> subinput
+  , output :: suboutput -> output
   }
 
 -- | a. Write a GADT to existentialise @subinput@ and @suboutput@.
 data NestedX input output where
   NestedX :: Nested input output subinput suboutput -> NestedX input output
 
--- | b. Write a function to "unpack" a NestedX. The user is going to have to
--- deal with all possible @subinput@ and @suboutput@ types.
+{- | b. Write a function to "unpack" a NestedX. The user is going to have to
+ deal with all possible @subinput@ and @suboutput@ types.
+-}
 unnestedX :: (forall si so. Nested i o si so -> r) -> NestedX i o -> r
 unnestedX f (NestedX nx) = f nx
 
--- | c. Why might we want to existentialise the subtypes away? What do we lose
--- by doing so? What do we gain?
+{- | c. Why might we want to existentialise the subtypes away? What do we lose
+ by doing so? What do we gain?
+-}
 
 -- From a global view, a component in a 'NestedX' black box should only expose
 -- its input and output types. Other details thus need to be abstracted away.
@@ -114,22 +124,25 @@ unnestedX f (NestedX nx) = f nx
 -- "will eventually be used somehow". Thanks to Haskell's laziness, storing a
 -- Box<dyn Into<String>> is ISOMORPHIC to storing a String.
 
--- | Let's continue with the theme of the last question. Let's say I have a few
--- HTML-renderable components:
+{- | Let's continue with the theme of the last question. Let's say I have a few
+ HTML-renderable components:
+-}
 data FirstGo input output
   = FText String
   | FHTML (String, String) [FirstGo input output]
 
 --       ^ properties     ^ children
 
--- | This is fine, but there's an issue: some functions only really apply to
--- 'FText' /or/ 'FHTML'. Now that this is a sum type, they'd have to result in
--- a 'Maybe'! Let's avoid this by splitting this sum type into separate types:
+{- | This is fine, but there's an issue: some functions only really apply to
+ 'FText' /or/ 'FHTML'. Now that this is a sum type, they'd have to result in
+ a 'Maybe'! Let's avoid this by splitting this sum type into separate types:
+-}
 newtype Text = Text String
 
--- | Uh oh! What's the type of our children? It could be either! In fact, it
--- could probably be anything that implements the following class, allowing us
--- to render our DSL to an HTML string:
+{- | Uh oh! What's the type of our children? It could be either! In fact, it
+ could probably be anything that implements the following class, allowing us
+ to render our DSL to an HTML string:
+-}
 class Renderable component where render :: component -> String
 
 -- | a. Write a type for the children.
@@ -137,14 +150,16 @@ data Child where Child :: Renderable r => r -> Child
 
 data HTML = HTML {properties :: (String, String), children :: [Child]}
 
--- | b. What I'd really like to do when rendering is 'fmap' over the children
--- with 'render'; what's stopping me? Fix it!
+{- | b. What I'd really like to do when rendering is 'fmap' over the children
+ with 'render'; what's stopping me? Fix it!
+-}
 instance Renderable Child where render (Child r) = render r
 
--- | c. Now that we're an established Haskell shop, we would /also/ like the
--- option to render our HTML to a Shakespeare template to write to a file
--- (http://hackage.haskell.org/package/shakespeare). How could we support this
--- new requirement with minimal code changes?
+{- | c. Now that we're an established Haskell shop, we would /also/ like the
+ option to render our HTML to a Shakespeare template to write to a file
+ (http://hackage.haskell.org/package/shakespeare). How could we support this
+ new requirement with minimal code changes?
+-}
 
 {- SIX -}
 
@@ -155,8 +170,9 @@ data MysteryBox a where
   StringBox :: String -> MysteryBox Int -> MysteryBox String
   BoolBox :: Bool -> MysteryBox String -> MysteryBox Bool
 
--- | a. Knowing what we now know about RankNTypes, we can write an 'unwrap'
--- function! Write the function, and don't be too upset if we need a 'Maybe'.
+{- | a. Knowing what we now know about RankNTypes, we can write an 'unwrap'
+ function! Write the function, and don't be too upset if we need a 'Maybe'.
+-}
 unwrapWith :: MysteryBox a -> (forall a. MysteryBox a -> r) -> Maybe r
 unwrapWith EmptyBox _ = Nothing
 unwrapWith (IntBox _ xs) f = Just $ f xs
@@ -167,8 +183,9 @@ unwrapWith (BoolBox _ xs) f = Just $ f xs
 
 -- We don't know how to unwrap an 'EmptyBox'.
 
--- | c. Write a function that uses 'unwrap' to print the name of the next
--- layer's constructor.
+{- | c. Write a function that uses 'unwrap' to print the name of the next
+ layer's constructor.
+-}
 layerName :: MysteryBox a -> String
 layerName EmptyBox = "EmptyBox"
 layerName (IntBox _ _) = "IntBox"
@@ -192,22 +209,25 @@ toNat :: SNat n -> Nat
 toNat SZ = Z
 toNat (SS n) = S $ toNat n
 
--- | How do we go the other way, though? How do we turn a 'Nat' into an 'SNat'?
--- In the general case, this is impossible: the 'Nat' could be calculated from
--- some user input, so we have no way of knowing what the 'SNat' type would be.
--- However, if we could have a function that would work /for all/ 'SNat'
--- values...
+{- | How do we go the other way, though? How do we turn a 'Nat' into an 'SNat'?
+ In the general case, this is impossible: the 'Nat' could be calculated from
+ some user input, so we have no way of knowing what the 'SNat' type would be.
+ However, if we could have a function that would work /for all/ 'SNat'
+ values...
+-}
 
--- | Implement the 'fromNat' function. It should take a 'Nat', along with some
--- SNat-accepting function (maybe at a higher rank?) that returns an @r@, and
--- then returns an @r@. The successor case is a bit weird here - type holes
--- will help you!
+{- | Implement the 'fromNat' function. It should take a 'Nat', along with some
+ SNat-accepting function (maybe at a higher rank?) that returns an @r@, and
+ then returns an @r@. The successor case is a bit weird here - type holes
+ will help you!
+-}
 fromNat :: Nat -> (forall n. SNat n -> r) -> r
 fromNat Z s2r = s2r SZ
 fromNat (S n) s2r = fromNat n (s2r . SS)
 
--- | If you're looking for a property that you could use to test your function,
--- remember that @fromNat x toNat === x@!
+{- | If you're looking for a property that you could use to test your function,
+ remember that @fromNat x toNat === x@!
+-}
 
 {- EIGHT -}
 
@@ -216,9 +236,10 @@ data Vector (n :: Nat) (a :: Type) where
   VNil :: Vector Z a
   VCons :: a -> Vector n a -> Vector (S n) a
 
--- | It would be nice to have a 'filter' function for vectors, but there's a
--- problem: we don't know at compile time what the new length of our vector
--- will be... but has that ever stopped us? Make it so!
+{- | It would be nice to have a 'filter' function for vectors, but there's a
+ problem: we don't know at compile time what the new length of our vector
+ will be... but has that ever stopped us? Make it so!
+-}
 vfilter :: (a -> Bool) -> Vector n a -> (forall m. Vector m a -> r) -> r
 vfilter _ VNil v2r = v2r VNil
 vfilter pred (VCons a as) v2r = vfilter pred as (if pred a then v2r . VCons a else v2r)
--- 06-TypeFamilies/src/Exercises.hs
+++ 06-TypeFamilies/src/Exercises.hs
@@ -3,12 +3,14 @@
 {-# LANGUAGE StandaloneKindSignatures #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE NoStarIsType #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE NoStarIsType #-}
 
 module Exercises where
 
 import Data.Kind (Constraint, Type)
+import Data.Type.Bool (If)
+import Data.Type.Equality (type (==))
 
 -- | Before we get started, let's talk about the @TypeOperators@ extension. All
 -- this does is allow us to write types whose names are operators, and write
@@ -18,6 +20,26 @@ import Data.Kind (Constraint, Type)
 {- ONE -}
 
 data Nat = Z | S Nat
+
+type One = S Z
+
+type Two = S One
+
+type Three = S Two
+
+type Four = S Three
+
+type Five = S Four
+
+type Six = S Five
+
+type Seven = S Six
+
+type Eight = S Seven
+
+type Nine = S Eight
+
+type Ten = S Nine
 
 -- | a. Use the @TypeOperators@ extension to rewrite the 'Add' family with the
 -- name '+':
@@ -36,9 +58,12 @@ data SNat value where
 type (*) :: Nat -> Nat -> Nat
 type family x * y where
   Z * _ = Z
-  (S x) * y = y + (x * y)
+  S x * y = y + (x * y)
 
 -- | c. Write a function to add two 'SNat' values.
+sadd :: SNat x -> SNat y -> SNat (x + y)
+sadd SZ y = y
+sadd (SS x) y = SS (x `sadd` y)
 
 {- TWO -}
 
@@ -49,43 +74,109 @@ data Vector count a where
 
 -- | a. Write a function that appends two vectors together. What would the size
 -- of the result be?
-
--- append :: Vector m a -> Vector n a -> Vector ??? a
+append :: Vector m a -> Vector n a -> Vector (m + n) a
+VNil `append` v = v
+VCons a as `append` bs = VCons a $ as `append` bs
 
 -- | b. Write a 'flatMap' function that takes a @Vector n a@, and a function
 -- @a -> Vector m b@, and produces a list that is the concatenation of these
 -- results. This could end up being a deceptively big job.
 
--- flatMap :: Vector n a -> (a -> Vector m b) -> Vector ??? b
-flatMap = error "Implement me!"
+-- This should've typechecked but didn't:
+-- flatMap :: (a -> Vector m b) -> Vector n a  -> Vector (m*n) b
+-- f `flatMap` VNil = VNil
+-- f `flatMap` VCons a as = (f a) `append` (f `flatMap` as)
+
+-- | This is a really interesting problem, and really exposes the problems we
+-- have in type-level programming: we can't convince GHC that @x + y == y + x@,
+-- or that @x + (y + z) == (x + y) + z@, without providing a very explicit
+-- proof. It just so happens that, if we define `*` with the successor's
+-- recursive step on the /right/ (as above), we're fine and don't need to do
+-- any of this hard work. Unfortunately, though, we'll regularly be less lucky.
+--
+-- This is irritating enough that libraries (or, rather, plugins) such as
+-- http://hackage.haskell.org/package/ghc-typelits-natnormalise exist purely to
+-- avoid these messes.
+flatMap :: Vector n a -> (a -> Vector m b) -> Vector (n * m) b
+flatMap VNil _ = VNil
+flatMap (VCons x xs) f = f x `append` flatMap xs f
 
 {- THREE -}
 
 -- | a. More boolean fun! Write the type-level @&&@ function for booleans.
+type (&&) :: Bool -> Bool -> Bool
+type family x && y where
+  True && True = True
+  _ && _ = False
 
 -- | b. Write the type-level @||@ function for booleans.
+type (||) :: Bool -> Bool -> Bool
+type family x || y where
+  False || False = False
+  _ || _ = True
 
 -- | c. Write an 'All' function that returns @'True@ if all the values in a
 -- type-level list of boleans are @'True@.
+type All :: [Bool] -> Bool
+type family All bs where
+  All '[] = True
+  All (b : bs) = b && All bs
 
 {- FOUR -}
 
 -- | a. Nat fun! Write a type-level 'compare' function using the promoted
 -- 'Ordering' type.
+type Compare :: Nat -> Nat -> Ordering
+type family x `Compare` y where
+  Z `Compare` Z = EQ
+  Z `Compare` _ = LT
+  _ `Compare` Z = GT
+  S x `Compare` S y = x `Compare` y
 
 -- | b. Write a 'Max' family to get the maximum of two natural numbers.
+type Max :: Nat -> Nat -> Nat
+type family x `Max` y where
+  x `Max` y = If (x `Compare` y == LT) y x
 
 -- | c. Write a family to get the maximum natural in a list.
+type Maximum :: [Nat] -> Nat
+type family Maximum xs where
+  Maximum '[] = Z
+  Maximum (x : xs) = x `Max` Maximum xs
 
 {- FIVE -}
 
 data Tree = Empty | Node Tree Nat Tree
 
 -- | Write a type family to insert a promoted 'Nat' into a promoted 'Tree'.
+type Insert :: Nat -> Tree -> Tree
+type family x `Insert` xs where
+  x `Insert` Empty = Node Empty x Empty
+  x `Insert` Node l y r =
+    If
+      (x `Compare` y == LT)
+      (Node (x `Insert` l) y r)
+      ( If
+          (x `Compare` y == GT)
+          (Node l y (x `Insert` r))
+          (Node l y r)
+      )
 
 {- SIX -}
 
 -- | Write a type family to /delete/ a promoted 'Nat' from a promoted 'Tree'.
+type Delete :: Nat -> Tree -> Tree
+type family x `Delete` xs where
+  x `Delete` Empty = Empty
+  x `Delete` Node Empty _ r = r
+  x `Delete` Node l x r = Node (TMaximum l `Delete` l) (TMaximum l) r
+  x `Delete` Node l y r = If (x `Compare` y == LT) (Node (x `Delete` l) y r) (Node l y (x `Delete` r))
+
+type TMaximum :: Tree -> Nat
+type family TMaximum xs where
+  TMaximum Empty = Z
+  TMaximum (Node _ x Empty) = x
+  TMaximum (Node _ _ r) = TMaximum r
 
 {- SEVEN -}
 
