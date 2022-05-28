3170020df7c9d4ffdc2a244bdc288cda1924b896
rami3l <rami3l@outlook.com>
2022-05-28 21:48:00 +0200

    feat: (wip) solve `09-MultiParamTypeClasses`, pt. 3

--- 09-MultiParamTypeClasses/src/Exercises.hs
+++ 09-MultiParamTypeClasses/src/Exercises.hs
@@ -13,12 +13,16 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Exercises where
 
 import Data.Kind (Constraint, Type)
 import Data.Map (Map)
 import Data.Proxy (Proxy (..))
+import Data.Type.Bool (type (&&))
+import Data.Type.Equality (type (==))
+import GHC.TypeLits (ErrorMessage (..), TypeError)
 
 {- ONE -}
 
@@ -113,7 +117,7 @@ instance Traversable Identity where
  extension does it suggest to fix this?
 -}
 type Wanderable :: ((Type -> Type) -> Constraint) -> (Type -> Type) -> Constraint
-class Wanderable c t where
+class Wanderable c t | t -> c where
   wander :: c f => (a -> f b) -> t a -> f (t b)
 -- ^ @{\-# LANGUAGE AllowAmbiguousTypes #-\}@ required.
 
@@ -136,9 +140,12 @@ instance Wanderable Applicative [] where
   /us/, this seems strange - we just want the list version! However, we
   haven't yet told GHC that there's only ever going to be one @Wanderable@
   instance for @[]@, so it will complain.
-  Thus, @TypeApplications@ must be used to disambiguate:
+  Thus, @TypeApplications@ must be used to indicate that we just want this instance:
   <https://gitlab.haskell.org/ghc/ghc/-/wikis/type-application> -}
   f `wander` (x : xs) = (:) <$> f x <*> wander @Applicative @[] f xs
+
+instance Wanderable Applicative Proxy where
+  _ `wander` _ = pure Proxy
 
 {- | d. Assuming you turned on the extension suggested by GHC, why does the
  following produce an error? Using only the extensions we've seen so far, how
@@ -147,36 +154,56 @@ instance Wanderable Applicative [] where
  we'll see in later chapters that there are neater solutions to this
  problem!)
 -}
+test = wander Just [1, 2, 3]
+{- ^ To disambiguate, @instance Wanderable Applicative []@ can be changed to @instance c ~ Applicative => Wanderable c []@.
+  However, a better solution is to use @FunctionalDependencies@.
+  ...or, if multiple possible instances for @Wanderable _ []@ is desired, @TypeApplication@ can be used:
+-}
 
--- test = wander Just [1, 2, 3]
+-- test = wander @Applicative Just [1, 2, 3]
 
 {- THREE -}
 
 data Nat = Z | S Nat
 
-data SNat (n :: Nat) where
-  SZ :: SNat 'Z
-  SS :: SNat n -> SNat ( 'S n)
+data SNat :: Nat -> Type where
+  SZ :: SNat Z
+  SS :: SNat n -> SNat (S n)
 
 {- | In the @DataKinds@ chapter, we wrote the 'SmallerThan' data type, which
  we'll call 'Fin' from now on:
+ Source: <https://agda.github.io/agda-stdlib/Data.Fin.Base.html#1203>
 -}
-data Fin (limit :: Nat) where
-  FZ :: Fin ( 'S n)
-  FS :: Fin n -> Fin ( 'S n)
+
+-- | Finite numbers: @[0..n-1]@.
+data Fin :: Nat -> Type where
+  FZ :: Fin (S n)
+  FS :: Fin n -> Fin (S n)
 
 {- | We can write a class to take an 'SNat' to a 'Fin' using
  @MultiParamTypeClasses@. We can even use @TypeOperators@ to give our class a
  more intuitive name:
 -}
-class (x :: Nat) < (y :: Nat) where
+type (<) :: Nat -> Nat -> Constraint
+class x < y where
   convert :: SNat x -> Fin y
+  coconvert :: Fin y -> Maybe (SNat x)
 
 -- | a. Write the instance that says @Z@ is smaller than @S n@ for /any/ @n@.
+instance Z < S n where
+  convert SZ = FZ
+
+  coconvert FZ = Just SZ
+  coconvert _ = Nothing
 
 {- | b. Write an instance that says, if @x@ is smaller than @y@, then @S x@ is
  smaller than @S y@.
 -}
+instance (x < y) => S x < S y where
+  convert (SS n) = FS $ convert n
+
+  coconvert FZ = Nothing
+  coconvert (FS n) = SS <$> coconvert n
 
 {- | c. Write the inverse function for the class definition and its two
  instances.
@@ -190,8 +217,14 @@ class (x :: Nat) < (y :: Nat) where
 -}
 
 -- | a. Write that typeclass!
+class TypeEquals (x :: Type) (y :: Type) where
+  to :: x -> y
+  from :: y -> x
 
 -- | b. Write that instance!
+instance x ~ y => TypeEquals x y where
+  from = id
+  to = id
 
 {- | c. When GHC sees @x ~ y@, it can apply anything it knows about @x@ to @y@,
  and vice versa. We don't have the same luxury with /our/ class, however –
@@ -206,10 +239,18 @@ class (x :: Nat) < (y :: Nat) where
  why? Can we even solve this?
 -}
 
+-- Duplicate instance declarations: @instance (x ~ y) => TypeEquals x y@
+-- instance (TypeEquals x y, TypeEquals y z) => TypeEquals x z where
+--   from = id
+--   to = id
+
 {- FIVE -}
 
 -- | It wouldn't be a proper chapter without an @HList@, would it?
-data HList (xs :: [Type])
+type HList :: [Type] -> Type
+data HList xs where
+  HNil :: HList '[]
+  HCons :: a -> HList as -> HList (a : as)
 
 -- In fact, you know what? You can definitely write an HList by now – I'll
 -- just put my feet up and wait here until you're done!
@@ -217,26 +258,38 @@ data HList (xs :: [Type])
 {- | Consider the following class for taking the given number of elements from
  the front of an HList:
 -}
-class HTake (n :: Nat) (xs :: [Type]) (ys :: [Type]) where
-  htake :: SNat n -> HList xs -> HList ys
+type HTake :: Nat -> [Type] -> [Type] -> Constraint
+class HTake n xs xs' where
+  htake :: SNat n -> HList xs -> HList xs'
 
 -- | a. Write an instance for taking 0 elements.
+instance HTake Z xs '[] where
+  htake SZ _ = HNil
 
 {- | b. Write an instance for taking a non-zero number. You "may" need a
  constraint on this instance.
 -}
+instance (HTake n xs xs') => HTake (S n) (x : xs) (x : xs') where
+  htake (SS n) (x `HCons` xs) = x `HCons` htake n xs
 
 -- | c. What case have we forgotten? How might we handle it?
+instance HTake n '[] '[] where
+  htake _ HNil = HNil
 
 {- SIX -}
 
 -- | We could also imagine a type class to "pluck" types out of @HList@:
-class Pluck (x :: Type) (xs :: [Type]) where
+type Pluck :: Type -> [Type] -> Constraint
+class Pluck x xs where
   pluck :: HList xs -> x
 
 -- | a. Write an instance for when the head of @xs@ is equal to @x@.
+instance Pluck x (x : xs) where
+  pluck (HCons x _) = x
 
 -- | b. Write an instance for when the head /isn't/ equal to @x@.
+instance {-# OVERLAPPABLE #-} Pluck x xs => Pluck x (y : xs) where
+  pluck (HCons y xs) = pluck xs
 
 {- | c. Using [the documentation for user-defined type
  errors](http://hackage.haskell.org/package/base-4.11.1.0/docs/GHC-TypeLits.html#g:4)
@@ -244,38 +297,70 @@ class Pluck (x :: Type) (xs :: [Type]) where
  through the entire @xs@ list (or started with an empty @HList@) and haven't
  found the type you're trying to find.
 -}
+instance
+  TypeError
+    ( Text "Plucked type ‘"
+        :<>: ShowType a
+        :<>: Text "’ not found in the list"
+    ) =>
+  Pluck a '[]
+  where
+  pluck = undefined
 
 {- | d. Making any changes required for your particular HList syntax, why
  doesn't the following work? Hint: try running @:t 3@ in GHCi.
 -}
-
--- mystery :: Int
--- mystery = pluck (HCons 3 HNil)
+mystery :: Int
+-- Answer: Integer literals are generic...
+-- mystery = pluck (HCons "world" (HCons 3 HNil))
+mystery = pluck (HCons "world" (HCons @Int 3 HNil))
 
 {- SEVEN -}
 
-{- | A variant is similar to an 'Either', but generalised to any non-zero
+{- | A variant is similar to an 'Either', but generalized to any non-zero
  number of parameters. Typically, we define it with two parameters: @Here@
  and @There@. These tell us which "position" our value inhabits:
 -}
-
--- variants :: [Variant '[Bool, Int, String]]
--- variants = [ Here True, There (Here 3), There (There (Here "hello")) ]
+variants :: [Variant [Bool, Int, String]]
+variants = [Here True, There (Here 3), There (There (Here "hello"))]
 
 -- | a. Write the 'Variant' type to make the above example compile.
-data Variant (xs :: [Type])
-
--- Here  :: ...
--- There :: ...
+type Variant :: [Type] -> Type
+data Variant xs where
+  Here :: x -> Variant (x : xs)
+  There :: Variant xs -> Variant (x : xs)
 
 {- | b. The example is /fine/, but there's a lot of 'Here'/'There' boilerplate.
  Wouldn't it be nice if we had a function that takes a type, and then returns
  you the value in the right position? Write it! If it works, the following
  should compile: @[inject True, inject (3 :: Int), inject "hello"]@.
 -}
+variants' :: [Variant [Bool, Int, String]]
+variants' = [inject True, inject @Int 3, inject "hello"]
 
--- class Inject … … where
---   inject :: …
+-- The following is for testing the compiling error message...
+-- variants'' :: [Variant [Bool, Int, String]]
+-- variants'' = [inject True, inject @Int 3, inject 'A']
+
+type Inject :: Type -> [Type] -> Constraint
+class Inject x xs where
+  inject :: x -> Variant xs
+
+instance Inject x (x : xs) where
+  inject = Here
+
+instance {-# OVERLAPPABLE #-} Inject x xs => Inject x (y : xs) where
+  inject = There . inject
+
+instance
+  TypeError
+    ( Text "Injected type ‘"
+        :<>: ShowType x
+        :<>: Text "’ not found in the list"
+    ) =>
+  Inject x '[]
+  where
+  inject = undefined
 
 {- | c. Why did we have to annotate the 3? This is getting frustrating... do
  you have any (not necessarily good) ideas on how we /could/ solve it?
@@ -376,14 +461,14 @@ instance Coat a Cold where doINeedACoat _ _ = True
 newtype UserId = UserId Int
 
 data User = User
-  { id :: UserId
+  { uid :: UserId
   , knownAs :: String
   }
 
 newtype CommentId = CommentId Int
 
 data Comment = Comment
-  { id :: CommentId
+  { uid :: CommentId
   , author :: UserId
   , text :: String
   }
